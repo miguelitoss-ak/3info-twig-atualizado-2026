a6e07dcba34e7e68b9a2ddbf517b94741039eec7
miguelitoss-ak <miguelakm07@gmail.com>
2026-04-06 17:09:20 -0300

    finalizardo Função Editar

--- jogos_editar.php
+++ jogos_editar.php
@@ -7,16 +7,42 @@ require('carregar_pdo.php');
 
 $erro = false;
 
-// if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-//     $id = (int) $_POST["id"] ?? false;
-//     if ($id) {
-//     $excluir = $pdo->prepare('UPDATE jogos SET (nome, estilo, capa) VALUES (?, ?, ?)');
-//     $excluir->bindParam(':id', $id);
-//     $excluir->execute();
-//     }
-//     header('location:jogos.php');
-//     die;
-// }
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id = (int) $_POST["id"] ?? false;
+    $nome = $_POST['nome'] ?? false;
+    $estilo = $_POST['estilo'] ?? false;
+
+
+    if (!$_FILES['capa']['error']) {
+
+        $dados = $pdo->prepare('SELECT capa FROM jogos WHERE id = :id');
+        $dados->execute([':id' => $id]);
+        $capa_velha = $dados->fetch(PDO::FETCH_ASSOC)['capa'];
+        $capa_velha = __DIR__ . '/img/' . $capa_velha;
+
+        if (file_exists($capa_velha)) {
+            unlink($capa_velha);
+        }
+
+        $ext = pathinfo($_FILES['capa']['name'], PATHINFO_EXTENSION);
+        $capa = uniqid() . '.' . $ext;
+        move_uploaded_file($_FILES['capa']['tmp_name'], "img/{$capa}");
+    }
+    $sql = 'UPDATE jogos SET nome = :nome , estilo = :estilo' . (isset($capa) ? ', capa = :capa' : '') . ' WHERE id = :id';
+    $dados = $pdo->prepare($sql);
+    $params = [
+        ':id' => $id,
+        ':nome' => $nome,
+        ':estilo' => $estilo,
+    ];
+    if (isset($capa)) {
+        $params[':capa'] = $capa;
+    }
+    $dados->execute($params);
+
+    header('location: jogos.php');
+    die;
+}
 
 $id = (int) $_GET["id"] ?? false;
 
